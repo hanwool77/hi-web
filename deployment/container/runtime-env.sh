1f3b47e00a2ff6c851e07d1b04fd548023f8c148
hanwool77 <wjsgksdnf11@naver.com>
2025-06-13 10:49:18 +0900

    빌드 오류 조치

--- deployment/container/runtime-env.sh
+++ deployment/container/runtime-env.sh
@@ -1,22 +1,14 @@
 #!/bin/sh
 
-# 환경 변수를 runtime-env.js에 주입
+# 환경 변수를 runtime-env.js에 주입 (hi-web 스타일)
 cat <<EOF > /usr/share/nginx/html/runtime-env.js
 window.__runtime_config__ = {
-  // Backend Services (External IP 사용)
-  AUTH_SERVICE_URL: '${AUTH_SERVICE_URL:-http://20.1.2.3:8081}',
-  MEMBER_SERVICE_URL: '${MEMBER_SERVICE_URL:-http://20.1.2.3:8081}',
-  STORE_SERVICE_URL: '${STORE_SERVICE_URL:-http://20.1.2.3:8082}',
-  REVIEW_SERVICE_URL: '${REVIEW_SERVICE_URL:-http://20.1.2.3:8083}',
-  ANALYTICS_SERVICE_URL: '${ANALYTICS_SERVICE_URL:-http://20.1.2.3:8084}',
-  RECOMMEND_SERVICE_URL: '${RECOMMEND_SERVICE_URL:-http://20.1.2.3:8085}',
-  
-  // API Gateway (Ingress Host 사용)
-  API_GATEWAY_URL: '${API_GATEWAY_URL:-http://hiorder.example.com}',
-  
-  // 앱 설정
-  APP_VERSION: '${APP_VERSION:-1.0.0}',
-  ENVIRONMENT: '${ENVIRONMENT:-production}'
+  AUTH_URL: '${AUTH_SERVICE_URL:-http://20.1.2.3/auth}',
+  MEMBER_URL: '${MEMBER_SERVICE_URL:-http://20.1.2.3/member}',
+  STORE_URL: '${STORE_SERVICE_URL:-http://20.1.2.3/store}', 
+  REVIEW_URL: '${REVIEW_SERVICE_URL:-http://20.1.2.3/review}',
+  ANALYTICS_URL: '${ANALYTICS_SERVICE_URL:-http://20.1.2.3/analytics}',
+  RECOMMEND_URL: '${RECOMMEND_SERVICE_URL:-http://20.1.2.3/recommend}'
 };
 EOF
 
