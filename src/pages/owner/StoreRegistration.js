f352823aefc58892d4c65ac7d13d04c6775d598a
haneuljeong98 <wjdgksmf107@gmail.com>
2025-06-17 14:41:10 +0900

    매장 등록 수정

--- src/pages/owner/StoreRegistration.js
+++ src/pages/owner/StoreRegistration.js
@@ -60,7 +60,7 @@ const StoreRegistration = () => {
     try {
       await storeService.createStore(formData);
       alert('매장이 등록되었습니다.');
-      navigate('/owner/store-registration');
+      navigate('/owner/stores-list');
     } catch (error) {
       console.error('매장 등록 실패:', error);
       setError(error.response?.data?.message || '매장 등록에 실패했습니다.');
